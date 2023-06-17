1b47dbb1376401b5fb0ff4fa390cdad43d0f6e7a
jakaria455173 <jakaria455173@gmail.com>
2023-06-17 19:52:04 +0600

    simple changed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     const toysRusCollection = client.db("ToysRus").collection("allToys");
 
     // get all toys data
@@ -179,4 +179,3 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Toy Marketplace Server Is Running On Port:http://localhost:${port}`);
 })
-
